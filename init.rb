9883c470c35010670cc45d2593a32b15cba41660
Jaap de Haan <jaap.dehaan@freenet.de>
2022-09-20 08:48:06 +0200

    fix: Redmine 5 compatibility (#2)
    * fix: Redmine 5 compatibility
    
    * fix: other zeitwerk fixes
    
    * fix: adapted ci for our purpose
    
    * fix: cleanup
    
    * fix: less aggressive CI checks
    
    * fix: make sure dependencies are defined before overriding/extending
    
    * fix: patch handling for rails 6

--- init.rb
+++ init.rb
@@ -1,24 +1,19 @@
-require 'redmine'
-require 'redmine_sudo/hooks'
+Rails.autoloaders.main.ignore("#{__dir__}/lib") if Rails::VERSION::MAJOR >= 6
 
-# Patches to existing classes/modules
-klass = defined?(ActiveSupport::Reloader) ? ActiveSupport::Reloader : ActionDispatch::Callbacks
-klass.to_prepare do
-  require_dependency 'redmine_sudo/user_patch'
-end
+require_relative 'lib/redmine_sudo/hooks'
 
 # Plugin generic informations
 Redmine::Plugin.register :redmine_sudo do
   name 'Redmine Sudo plugin'
   description 'This plugin gives sudo-like powers to Redmine administrators'
-  author 'Jean-Baptiste BARTH'
+  author 'Jean-Baptiste BARTH (orig)'
   author_url 'mailto:jeanbaptiste.barth@gmail.com'
-  url 'https://github.com/jbbarth/redmine_sudo'
-  version '0.0.1'
-  requires_redmine :version_or_higher => '2.5.0'
-  requires_redmine_plugin :redmine_base_rspec, :version_or_higher => '0.0.3' if Rails.env.test?
-  requires_redmine_plugin :redmine_base_deface, :version_or_higher => '0.0.1'
-  settings :default => {
+  url 'https://github.com/tools-aoeur/redmine_sudo'
+  version '1.0.0'
+  requires_redmine version_or_higher: '4.0.0'
+  requires_redmine_plugin :redmine_base_rspec, version_or_higher: '0.0.3' if Rails.env.test?
+  requires_redmine_plugin :redmine_base_deface, version_or_higher: '0.0.1'
+  settings default: {
     'become_admin' => '[sudo -v]',
     'become_user' => '[sudo -k]',
     'additional_css' => "#top-menu { background-color:#BA0C04; }\n
@@ -27,5 +22,18 @@ Redmine::Plugin.register :redmine_sudo do
                         #main-menu li a.new-object { background-color:#BA0C04; }\n
                         #main-menu li a:hover { background-color:#8D0A02; }\n
                         @media all and (max-width: 899px) { #header{ background-color: #dd0037 !important; }" },
-           :partial => 'settings/redmine_sudo_settings'
+           partial: 'settings/redmine_sudo_settings'
+end
+
+# Patches to existing classes/modules
+if Rails::VERSION::MAJOR < 6
+  klass = defined?(ActiveSupport::Reloader) ? ActiveSupport::Reloader : ActionDispatch::Callbacks
+  klass.to_prepare do
+    require_relative 'lib/redmine_sudo/user_patch'
+  end
+else
+  # see https://www.redmine.org/issues/36245#note-11 and following for changes with zeitwerk autoloading
+  Rails.application.config.after_initialize do
+    require_relative 'lib/redmine_sudo/user_patch'
+  end
 end

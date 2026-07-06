85008a921d865fe72980649507da019c58a9b488
Jaap de Haan <261428+jdehaan@users.noreply.github.com>
2026-07-06 17:18:12 +0200

    feat: show sudoer column by default next to admin in Users list

--- lib/redmine_sudo/user_query_patch.rb
+++ lib/redmine_sudo/user_query_patch.rb
@@ -7,4 +7,16 @@ module RedmineSudo::UserQueryPatch
       type: :list,
       values: [[l(:general_text_yes), '1'], [l(:general_text_no), '0']]
   end
+
+  # Shows the `sudoer` column right next to `admin` by default, in addition to
+  # core's own defaults.
+  def default_columns_names
+    names = super.dup
+    admin_index = names.index(:admin)
+    if admin_index && !names.include?(:sudoer)
+      names.insert(admin_index + 1, :sudoer)
+    end
+    names
+  end
 end
+
